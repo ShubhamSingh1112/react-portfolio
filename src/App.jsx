e34f47479f08974450e4475a84ee54f69d9e9f08
SHubhamSingh1112 <singhshubhamsingh2208@outlook.com>
2023-01-30 23:59:15 +0530

    Files addition

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
-// import Button from '@mui/material/Button';
+import Button from '@mui/material/Button';
+import { Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from '../src/components/Navbar';
-import './App.css';
+import Home from "../src/pages/Home";
+import About from "../src/pages/About";
+import Portfolio from "../src/pages/Portfolio";
+import Contact from "../src/pages/Contact";
+
 
 function App() {
   return (
     <div className="App">
-      {/* <Button variant="contained">Hello World</Button> */}
+      <Button variant="contained">Hello World</Button>
       <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="About" element={<About />} />
+          <Route path="Portfolio" element={<Portfolio />} />
+          <Route path="Contact" element={<Contact />} />
+        </Routes>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 
 export default class Navbar extends Component {
   render() {
     return (
-      <div>Navbar</div>
+      <div className="">
+        <Link className="" to="/">Home</Link>
+        <Link className="" to="/About">About</Link>
+        <Link className="" to="/Portfolio">Portfolio</Link>
+        <Link className="" to="/Contact">Contact</Link>
+      </div>
     )
   }
 }
