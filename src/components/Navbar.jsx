e22ac4e182d1ffc71b45728588c074db6ab3c730
SHubhamSingh1112 <singhshubhamsingh2208@outlook.com>
2023-01-31 23:59:05 +0530

    Navbar properties addition

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import Box from '@mui/material/Box';
 
 export default class Navbar extends Component {
   render() {
     return (
-      <div className="">
+        <Box
+      sx={{
+        width: '100%',
+        height: 'auo',
+        backgroundColor: 'primary.dark',
+        '&:hover': {
+          backgroundColor: 'primary.main',
+          opacity: [0.9, 0.8, 0.7],
+        },
+      }}
+    >
         <Link className="" to="/">Home</Link>
         <Link className="" to="/About">About</Link>
         <Link className="" to="/Portfolio">Portfolio</Link>
         <Link className="" to="/Contact">Contact</Link>
-      </div>
+      </Box>
     )
   }
 }
+
+
