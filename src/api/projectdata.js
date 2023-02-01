625f7b6744e2eab123f8c31cee84e36cd77dfd30
SHubhamSingh1112 <singhshubhamsingh2208@outlook.com>
2023-02-01 23:58:08 +0530

    Project Data Added

---
+++ src/api/projectdata.js
@@ -0,0 +1,18 @@
+export const projects = [
+  {
+    title: "Cryptocurrencies List App",
+    subtitle: "React App + CoinStats API",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: "https://candid-belekoy-19ce6e.netlify.app/",
+  },
+  {
+    title: "React Gallery App",
+    subtitle: "React Image Search Using Unsplash API.",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "https://silver-faloodeh-032cab.netlify.app/",
+  },
+];
