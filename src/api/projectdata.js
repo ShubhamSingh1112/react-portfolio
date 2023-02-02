ad25bb0f90b86cd78a02fda4f8488df7771a0e9a
SHubhamSingh1112 <singhshubhamsingh2208@outlook.com>
2023-02-02 23:58:37 +0530

    Project Updates

--- src/api/projectdata.js
+++ src/api/projectdata.js
@@ -15,4 +15,20 @@ export const projects = [
     image: "./project-2.gif",
     link: "https://silver-faloodeh-032cab.netlify.app/",
   },
+  {
+    title: "Weather App",
+    subtitle: "React App + Openweathermap API",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://sweet-palmier-e326fd.netlify.app/",
+  },
+  {
+    title: "RECIPE APP",
+    subtitle: "React Recipe App Using Edamami API.",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://aesthetic-shortbread-37b4f3.netlify.app/",
+  },
 ];
