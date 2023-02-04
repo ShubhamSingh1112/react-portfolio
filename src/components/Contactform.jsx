a7448c847fdbe6d368cd6d11c2aea71de05e7099
SHubhamSingh1112 <singhshubhamsingh2208@outlook.com>
2023-02-04 22:58:09 +0530

    Adding Email

--- src/components/Contactform.jsx
+++ src/components/Contactform.jsx
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
+import Contactus from './Contactus'
 
 export default class Contactform extends Component {
   render() {
     return (
-      <div>Contactform</div>
+      <>
+      <Container maxWidth="sm">
+        <Box>
+          <h2>Contact Me</h2>
+          <Contactus />
+        </Box>
+      </Container>
+      </>
     )
   }
 }
---
+++ src/components/Contactus.jsx
@@ -0,0 +1,31 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+export const ContactUs = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_v8r55el', 'template_h1eu8jd', form.current, 'hThoRugJLEbVPDfY-')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" required />
+      <label>Email</label>
+      <input type="email" name="user_email" required />
+      <label>Message</label>
+      <textarea name="message" required />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
+
+export default ContactUs
